326f9aa69c14742104f577e0929f6f5a341b33f8
murakami7581 <yoshi7614ya@icloud.com>
2021-08-30 19:10:44 +0900

    仮

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <footer class="l-footer">
  <div class="p-footer__container">
-
-  <?php wp_nav_menu( array( 
-   'theme_location' => 'footer' 
-   ) ); 
-   ?>
+<ul>
+ <li><a href="<?php echo get_page_link(550); ?>">ショップ情報</a></li>
+ <li><a href="<?php echo get_page_link(79); ?>">ヒストリー</a></li>
+</ul>
+	<p class="p-footer__container__subtext"><small>Copyright: RaiseTech</small></p>
  </div>
 </footer>
 <?php wp_footer(); ?>
--- single.php
+++ single.php
@@ -1,6 +1,15 @@
 <?php get_header(); ?>
 
-<?php get_template_part('section') ?>
+
+<div class="p-main">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<h1><?php the_title(); ?></h1>
+<div class="p-main__section1">
+    <?php the_content(); ?>
+</div>
+<?php endwhile; endif; ?>
+</div>
+</article>	
 
 <?php get_sidebar(); ?>
 
