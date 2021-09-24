b08a9ad5da7a9404ff5445d311ff118bf6f8e6cd
murakami7581 <yoshi7614ya@icloud.com>
2021-09-24 15:19:20 +0900

    課題

--- front-page.php
+++ front-page.php
@@ -4,6 +4,7 @@
   <div class="c-main__container">
    <div class="c-main__container__top">
     <img class="c-main__container__img1" src="<?php echo esc_url( get_template_directory_uri() . "/image/main_img.png" ); ?>" alt="一番上の写真">
+    <img class="c-main__container__img1sp" src="<?php echo esc_url( get_template_directory_uri() . "/image/mainvisual-sp.png" ); ?>" alt="一番上の写真">
     <p class="c-main__container__text">ダミーサイト</p>
    </div>
   
@@ -41,18 +42,18 @@
       </div>
     </div>
    
-
-    <div class="p-section__map">
-     <div class="p-section__map2"></div>
-      <div class="p-section__map3">
-       <h2 class="p-section__map3__text">見出しが入ります</h2>
-       <p class="p-section__map3__subtext">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+<div class="p-section__map">
+  <iframe src="https://maps.google.co.jp/maps?output=embed&z=18&ll=35.658608938829000,139.74324421519987&q=東京タワー" width="550" height="300" frameborder="0" style="border:0;width:100%;filter:brightness(70%);-webkit-filter:brightness(70%)" allowfullscreen></iframe>
+     <div class="p-section__map1"></div>
+      <div class="p-section__map2">
+       <h2 class="p-section__map2__text">見出しが入ります</h2>
+       <p class="p-section__map2__subtext">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
        </p>
-      </div>
-    </div>
+</div>
    </div>
  </div>
-
+ </div>
+    
 </article>
 <!----- メインコンテンツ END --- -->
 
